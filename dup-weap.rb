e9889dfba5f1d8000bf188c3335c49dbd892a87e
Jim Nelson <jim@midnightfreddie.com>
2016-01-24 09:57:48 -0600

    Inventory fetching working

--- dup-weap.rb
+++ dup-weap.rb
@@ -11,13 +11,13 @@ def get_items(steam_id = ENV['STEAM_ID'], steam_api_key = ENV['STEAM_API_KEY'], 
   raise 'Need to provide Steam ID' if steam_id.nil?
   # Inventory
   uri = URI("http://api.steampowered.com/IEconItems_#{app_id}/GetPlayerItems/v0001/?key=#{steam_api_key}&SteamID=#{steam_id}")
-  result = Net::HTTP(uri)
-  raise 'Failure requesting inventory' unless result.is_a?(Net:HTTPSuccess)
+  result = Net::HTTP.get_response(uri)
+  raise 'Failure requesting inventory' unless result.is_a?(Net::HTTPSuccess)
   File.write('./inv.json', result.body)
   # Schema
   uri = URI("http://api.steampowered.com/IEconItems_#{app_id}/GetSchema/v0001/?key=#{steam_api_key}")
-  result = Net::HTTP(uri)
-  raise 'Failure requesting inventory' unless result.is_a?(Net:HTTPSuccess)
+  result = Net::HTTP.get_response(uri)
+  raise 'Failure requesting inventory' unless result.is_a?(Net::HTTPSuccess)
   File.write('./schema.json', result.body)
 end
 
