fdb79c0dd24317ed7f0db9f17b8b40c2e1252d81
Jim Nelson <jim@midnightfreddie.com>
2016-01-25 00:34:26 -0600

    Including quality

--- dup-weap.rb
+++ dup-weap.rb
@@ -31,16 +31,23 @@ end
 items = JSON.parse(File.read(ITEMS_FILE))
 schema = JSON.parse(File.read(SCHEMA_FILE))
 
-# Build lookup hash table for item descriptions
+# Build lookup hash table for item descriptions because array index is sparse
 defindex = Hash.new
 schema["result"]["items"].each do | item |
   defindex[item["defindex"]] = item
 end
 
+quality = Hash.new
+schema["result"]["qualities"].each do | key, value |
+  quality[value] = key
+end
+
 items["result"]["items"]
   .select { | item | defindex[item["defindex"]]["craft_class"].eql? "weapon"}
+  .sort_by { | item | item["quality"] }
+  .reverse
   .sort_by { | item | item["defindex"] }
   .each do | item |
     defitem = defindex[item["defindex"]]
-    puts "Level #{item["level"]} #{defitem["name"]} #{defitem["craft_class"]}"
+    puts "#{quality[item["quality"]]} #{item["quality"]} Level #{item["level"]} #{defitem["name"]}"
   end
