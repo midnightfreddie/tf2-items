c3937ada261af0dcd6ee5f48ff38dda1813081f0
Jim Nelson <jim@midnightfreddie.com>
2016-02-07 21:28:28 -0600

    Cleanup

--- dup-weap.rb
+++ dup-weap.rb
@@ -2,9 +2,6 @@ require './steam_inventory'
 
 tf2 = SteamInventory::Items.new
 
-puts tf2.out_text(tf2.dup_weap)
-puts ""
 puts tf2.out_text(tf2.scrap_weap)
 
-# File.write('out.html',tf2.out_html(tf2.dup_weap))
 File.write('out.html',tf2.out_html(tf2.scrap_weap, "Possibly Scrappable Weapons"))
